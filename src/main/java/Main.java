f38659aae2c79ef66853f81d556d9c5f8deb2ea0
opesnoir <goodsurroundsme@outlook.com>
2023-03-02 09:49:20 +0100

    edhub opdracht 1 variabelen toewijzen af en nagekeken

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,15 @@ public class Main {
     public static void main(String[] args) {
 
         String string_voorbeeld = "Dit is een voorbeeld";
+        String string_hello_world ="hello world";
+        //Het is conventie om variabelen die een boolean bevatten een naam te geven die begint met "-is" of "-should". Ook is het gebruikelijk de benaming altijd volgens het format is <Positive> te beschrijven, om te voorkomen dat we dubbele ontkenningen gebruiken: boolean isComplete = false;.
+        Boolean boolean_true = true;
 
+        int int_four = 4;
+        short short_minus_eight = -8;
+        //achter float plaats je een f om een error te voorkomen, dit noem je postfix. Een long heeft ook een postfix, maar dat is een L.
+        float float_six_point_five = 6.5f;
+        double double_minus_two_point_three = -2.3;
 
 
 
@@ -19,8 +27,10 @@ public class Main {
         System.out.println(double_minus_two_point_three); // double -2.3
 
         // Bonus: Wijs een nieuwe waarde toe aan een bestaande variabele
-
+        string_voorbeeld = "Why are you not printing? Or are you going to? Yeah, you are printing. That is great!";
         System.out.println(string_voorbeeld); // String Dit is een aangepast voorbeeld
+
+
     }
 
 }
